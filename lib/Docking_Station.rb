4dca4577a61bf64cbd07fa53d8e895b1ff10eadb
Marwa Elali <marwaelali@gmail.com>
2018-06-15 22:45:05 +0100

    syntax refactoring

--- lib/Docking_Station.rb
+++ lib/Docking_Station.rb
@@ -7,13 +7,13 @@ class DockingStation
   end
 
   def release_bike
-    fail "No bikes available" if @bikes.length == 0
+    fail 'No bikes available' if @bikes.length == 0
     @bike
   end
 
   def dock_bike(bike)
     @bike = bike
-    fail "Docking Station is full!" if @bikes.length >= 20
+    fail 'Docking Station is full!' if @bikes.length >= 20
     @bikes.push(@bike)
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,44 +1,44 @@
-require "docking_station"
-require "bike"
+require 'docking_station'
+require 'bike'
 
 describe DockingStation do
 
-  it "expected to respond to release_bike" do
+  it 'expected to respond to release_bike' do
    is_expected.to respond_to(:release_bike)
   end
 
-  it "gets a bike when passed release_bike" do
+  it 'gets a bike when passed release_bike' do
     station = DockingStation.new
     station.dock_bike(Bike.new)
    expect(station.release_bike).to be_an_instance_of(Bike)
   end
 
-  it "returns true if bike is working" do
+  it 'returns true if bike is working' do
     station = DockingStation.new
     station.dock_bike(Bike.new)
     expect(station.release_bike.working?).to be(true).or be(false)
   end
 
-  it "expected to respond to dock_bike" do
+  it 'expected to respond to dock_bike' do
      is_expected.to respond_to(:dock_bike)
   end
 
-  it "expected to respond to dock_bike" do
+  it 'expected to respond to dock_bike' do
      expect(DockingStation.new).to respond_to(:dock_bike).with(1).argument
   end
 
-  it "should raise an error when docking_station is full" do
+  it 'should raise an error when docking_station is full' do
     station = DockingStation.new
      20.times { station.dock_bike(Bike.new)}
-    expect{ station.dock_bike(Bike.new) }.to raise_error("Docking Station is full!")
+    expect{ station.dock_bike(Bike.new) }.to raise_error('Docking Station is full!')
   end
 
-  it "should raise an error when bikes are not available" do
+  it 'should raise an error when bikes are not available' do
     station = DockingStation.new
-    expect{ station.release_bike }.to raise_error("No bikes available")
+    expect{ station.release_bike }.to raise_error('No bikes available')
   end
 
-  it "should return a bike when a bike is available" do
+  it 'should return a bike when a bike is available' do
     docking_station = DockingStation.new
     docking_station.dock_bike(Bike.new)
     expect(docking_station.release_bike).to be_an_instance_of(Bike)
